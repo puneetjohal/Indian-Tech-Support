477c1fb3aab2e22727e88b5b896f5583a89c0a84
Ahnaf Hasan <ahasan1@stuy.edu>
2018-10-18 13:10:33 -0400

    add changes to templates and app.py to be able to use sqlite3

--- Current Version/app.py
+++ Current Version/app.py
@@ -1,15 +1,15 @@
 from flask import Flask,render_template,session,request,url_for,redirect,flash
-import os
+import os, sqlite3, csv
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
-#hardcoded user
-user1 = "tester"
-pass1 = "test"
-
+DATABASE = "./Big_Daddy.db"
+db = sqlite3.connect(DATABASE)
+c = db.cursor()
+c.execute("CREATE TABLE IF NOT EXISTS logins (username TEXT, password TEXT)")
 #checks the credentials against the hard coded user
-def check_credentials(username,password):
-    return username == user1 and password == pass1
+#def check_credentials(username,password):
+ #   return username == user1 and password == pass1
 
 @app.route('/')
 def home():
@@ -24,24 +24,36 @@ def logout():
     #redirects to the home page after logging user out
     return redirect(url_for('home'))
 
+@app.route('/login')
+def log():
+    return render_template("login.html")
+
 @app.route("/auth",methods=["POST"])
 def authenticate():
-    error = ""
-    username = request.form['username']
-    password = request.form['password']
-    #if the credentials match, add the user to session
-    if check_credentials(username,password):
-        session["user"] = username
-    #checks for bad username
-    else:
-        error = "Unknown user, please try again"
-        #flashes error
-        flash(error)
-    #redirects to home
+    db = sqlite3.connect(DATABASE)
+    c = db.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS logins(username TEXT, password TEXT)")
+    c.execute("INSERT INTO logins VALUES(\"asd\",\"asd\")")
+    c.execute("INSERT INTO logins VALUES(\"val\",\"val\")")
+    c.execute("SELECT * FROM logins")
+    usrs = c.fetchone()
+
+    while usrs != None:
+        print(usrs[0] + "," + usrs[1])
+        if usrs[0] == request.form.get('username') and usrs[1] == request.form.get('password'):
+            db.commit()
+            db.close()
+            return redirect(url_for('log'))
+        else:
+            usrs = c.fetchone()
+    print("Nothing")
+    db.commit()
+    db.close()
     return redirect(url_for('home'))
+
 @app.route("/home")
 def blogHome():
-
+    return render_template("base.html", title = "SUCCESS", content = "YAT", footer = "YAT")
 
 
 
